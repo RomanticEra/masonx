7526218ca7e24af46458538499d51eb2a9d7842e
huang12zheng <805104533@qq.com>
2021-11-26 23:19:32 +0800

    feat: 🚸  extend from mason fix: #1 #2

--- bin/masonx.dart
+++ bin/masonx.dart
@@ -1,11 +1,10 @@
+import 'package:mason/src/command_runner.dart' show MasonCommandRunner;
 import 'package:masonx/masonx.dart';
-import 'package:masonx/src/commands/util.dart';
 
 Future<void> main(List<String> args) async {
   try {
     await masonx.run(args);
   } catch (e) {
-    logger.err(e.toString());
-    rethrow;
+    await MasonCommandRunner().run(args);
   }
 }
